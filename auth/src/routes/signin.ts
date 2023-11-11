94a387a09ddfb1598a329b973e8770c4bb82340a
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2023-11-12 00:59:56 +0530

    Updated import statements

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,8 @@ import { sign } from "jsonwebtoken";
 
 import { Password } from "../services/password";
 import { User } from "../models/User";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@ticketingdotcom/common";
+
 const router = express.Router();
 
 router.post(
